79ecd2e03ddcacb920fdcc11d163e0f3e34fe9c9
Jyeshtha Prabhu <prabhu.j@northeastern.edu>
2024-03-30 06:54:51 +0000

    changes

--- models/mymodel.sql
+++
@@ -1,4 +0,0 @@
--- models/my_model.sql
-SELECT *
-FROM {{ source('raw_data_source', 'CAPITAL_AND_OTHER_SELECTED_BALANCE_SHEET_ITEMS') }}
--- Replace 'CAPITAL_AND_OTHER_SELECTED_BALANCE_SHEET_ITEMS' with the name of the table you want to query
