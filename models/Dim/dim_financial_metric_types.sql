57ed24094bb7f6469383a219c01b6faeff5a2ebf
Jyeshtha Prabhu <prabhu.j@northeastern.edu>
2024-04-06 00:20:26 +0000

    modifying model

--- models/Dim/dim_financial_metric_types.sql
+++ models/Dim/dim_financial_metric_types.sql
@@ -1,22 +1,13 @@
 {{ config(materialized='table') }}
 
-with source_data as (
-    -- Replace this with the actual extraction from your source table
-    select distinct metric_name, description
-    from {{ ref('Consolidated_financial_highlights') }}  -- Adjust this to your actual source table reference
+WITH metric_types AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY metric_name) AS metric_type_id,
+        metric_name
+    FROM {{ ref('financial_metric_types') }}
 )
 
-, ranked_metrics as (
-    select
-        row_number() over (order by metric_name) as metric_type_id,
-        metric_name,
-        description
-    from source_data
-)
-
-select
+SELECT
     metric_type_id,
-    metric_name as metric_type,
-    description
-from ranked_metrics
-
+    metric_name AS metric_type
+FROM metric_types
